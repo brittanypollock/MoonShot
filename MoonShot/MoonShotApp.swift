67aa86a03bb0c1b8499143923aa7c65578d5cb0a
Brittany Pollock <brittpollock15@gmail.com>
2026-02-19 18:19:56 -0800

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Brittany Pollock on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
