a88c9f538a946549ae2ca802cfd7239e1b43b74d
Brittany Pollock <brittpollock15@gmail.com>
2026-02-22 13:39:44 -0800

    Loading in JSON code and decoding it.

---
+++ MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Brittany Pollock on 2/22/26.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
