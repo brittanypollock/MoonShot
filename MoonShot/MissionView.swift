0813e3480887f85fe79fb87c491515264fd6dfb3
Brittany Pollock <brittpollock15@gmail.com>
2026-02-23 18:38:53 -0800

    Created MissionView.swift

---
+++ MoonShot/MissionView.swift
@@ -0,0 +1,45 @@
+//
+//  MissionView.swift
+//  MoonShot
+//
+//  Created by Brittany Pollock on 2/23/26.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    let mission: Mission
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { width, axis in
+                        width * 0.6
+                    }
+                
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                    
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    MissionView(mission: missions[0])
+        .preferredColorScheme(.dark)
+}
